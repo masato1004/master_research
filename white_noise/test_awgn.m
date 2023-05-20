ab16896971379b00f015bd02a0930fc3c4493982
Masato <masato_7121@keio.jp>
2023-05-21 00:05:00 +0900

    making noise

--- white_noise/test_awgn.m
+++ white_noise/test_awgn.m
@@ -3,7 +3,10 @@ close all;
 x1 = 0:0.01:10;
 x1 = reshape(x1,size(x1));
 y1 = sin(x1);
+noise = sqrt(0.05) * randn(size(x1));
 
-y1 = awgn(y1,16,'measured');
+y1 = y1 + noise;
+% y1 = awgn(y1,16,'measured');
 
 plot(x1,y1);
+grid on;
