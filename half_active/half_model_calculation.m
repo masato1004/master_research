c017fdfde7adfc900d183635fdcb9043e875d652
INOUE MASATO <masato_7121@keio.jp>
2023-05-27 00:31:54 +0900

    Now on Changing: error of plane-detection

--- half_active/half_model_calculation.m
+++ half_active/half_model_calculation.m
@@ -88,7 +88,8 @@ for i=1:c-1
         end
         if low_freq_noise
             sig = 3-2*randi(2);
-            lnoise = sig*lsd(wf_local(1,:))+(lsd(wf_local(1,:))./3).*randn(size(wf_local(2,:)));
+            % lnoise = sig*lsd(wf_local(1,:))+(lsd(wf_local(1,:))./3).*randn(size(wf_local(2,:)));
+            lnoise = sig*
             wf_local(1,:) = wf_local(1,:) + lnoise;
             [~,ind] = sort(wf_local(1,:));
             wf_local= wf_local(:,ind);
