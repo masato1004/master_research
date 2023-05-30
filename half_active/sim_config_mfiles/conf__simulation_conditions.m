78cc9b0292e4c701f266356fa459c7c5dd6d77d3
INOUE MASATO <masato_7121@keio.jp>
2023-05-30 16:40:58 +0900

    Edit

--- half_active/sim_config_mfiles/conf__simulation_conditions.m
+++ half_active/sim_config_mfiles/conf__simulation_conditions.m
@@ -1,15 +1,15 @@
 %% Dfine simulation condition with boolean
 % draw an animation or not
 animation = false;
-prev_anim = false;
+prev_anim = true;
 
 % add noise or not
 high_freq_noise = true;
 low_freq_noise  = true;
 
 % spatial smoothing method
-wa  = true;
-lpf = true;
+wa  = false;
+lpf = false;
 
 % control method
 passive = false;
