5a99bdbbbfba8672aefa40b90deba13641fff699
INOUE MASATO <masato_7121@keio.jp>
2023-06-13 17:36:22 +0900

    results checker

--- half_active/monte_carlo/check_monte_results.m
+++ half_active/monte_carlo/check_monte_results.m
@@ -1,4 +1,5 @@
 pitch_inte_list = pitch_inte_list(pitch_inte_list(:,:)~=0);
 pitch_max_list = pitch_max_list(pitch_max_list(:,:)~=0);
 input_inte_list = input_inte_list(input_inte_list(:,:)~=0);
-input_max_list = input_max_list(input_max_list(:,:)~=0);
+input_max_list = input_max_list(input_max_list(:,:)~=0);   % 500*1
+
