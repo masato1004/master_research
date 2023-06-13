529b72d608a58a46ec5f0c18def2326d52cd9534
INOUE MASATO <masato_7121@keio.jp>
2023-06-13 19:46:52 +0900

    Add: drawer

--- half_active/monte_carlo/check_monte_results.m
+++ half_active/monte_carlo/check_monte_results.m
@@ -8,6 +8,7 @@ max_z0_list = [0.02, 0.03, 0.04, 0.05, 0.08];
 % mat file folder
 smoothed_dir = "controled/";
 nonsmoothed_dir = "results/";
+onlylpf_dir = "onlylpf/";
 
 % loading and drawing
 for i = 1:5
@@ -19,6 +20,7 @@ for i = 1:5
             load_name = "ld="+ld+"-max_z0="+max_z0;
             load(smoothed_dir+load_name);
             load(nonsmoothed_dir+load_name);
+            load(onlylpf_dir+load_name);
 
             % resize because of miscoding (500*500 -> 500*1)
             pitch_inte_list = pitch_inte_list(pitch_inte_list(:,:)~=0);
@@ -29,23 +31,25 @@ for i = 1:5
             inte_fig = figure('name',"inte-"+load_name+".mat");
             scatter(input_inte_list,pitch_inte_list,20,[1,0,0].*ones(size(input_inte_list))); hold on; grid on; box on;
             scatter(input_inte_list_nonfilter,pitch_inte_list_nonfilter,20,[0,0,1].*ones(size(input_inte_list)));
+            scatter(input_inte_list_onlylpf,pitch_inte_list_onlylpf,20,[0,0,1].*ones(size(input_inte_list)));
             ylabel("Integrations of Pitch Angle");
             xlabel("Integrations of Control Input");
+            legend("Proposed","Non smoothed","Only LPF");
             fontname(inte_fig,"Times New Roman");
             fontsize(inte_fig,9.5,"points");
 
             max_fig = figure('name',"max-"+load_name+".mat");
             scatter(input_max_list,pitch_max_list,20,[1,0,0].*ones(size(input_max_list))); hold on; grid on; box on;
-            scatter(input_max_list_nonfilter,pitch_max_list_nonfilter,20,[0,0,1].*ones(size(input_max_list)));
+            scatter(input_max_list_nonfilter,pitch_max_list_nonfilter,20,[0,0,0].*ones(size(input_max_list)));
+            scatter(input_max_list_onlylpf,pitch_max_list_onlylpf,20,[0,0,0].*ones(size(input_max_list)));
             ylabel("Maximums of Pitch Angle [rad]");
             xlabel("Maximums of Control Input [N]");
+            legend("Proposed","Non smoothed","Only LPF");
             fontname(max_fig,"Times New Roman");
             fontsize(max_fig,9.5,"points");
 
-            saveas(inte_fig,"figs/smoothed/inte-"+load_name+".fig");
-            saveas(max_fig,"figs/smoothed/max-"+load_name+".fig");
-            saveas(inte_fig,"figs/nonsmoothed/inte-"+load_name+".fig");
-            saveas(max_fig,"figs/nonsmoothed/max-"+load_name+".fig");
+            saveas(inte_fig,"figs/inte-"+load_name+".fig");
+            saveas(max_fig,"figs/max-"+load_name+".fig");
         end
     end
 end
