4213681f8538d953b1bd43998f87f95b5234bc3a
INOUE MASATO <masato@yt.sd.keio.ac.jp>
2023-06-14 18:54:31 +0900

    Add: monte carlo of results

--- half_active/monte_carlo/monte_sim.m
+++ half_active/monte_carlo/monte_sim.m
@@ -38,20 +38,20 @@ for con = 1:width(ld_list)
         input_inte_list_nonfilter(loop) = input_integral;
         input_max_list_nonfilter(loop) = input_max;
         
-        run("half_model_calculation.m")
-        pitch_inte_list_onlylpf(loop) = pitch_integral;
-        pitch_max_list_onlylpf(loop) = pitch_max;
-        input_inte_list_onlylpf(loop) = input_integral;
-        input_max_list_onlylpf(loop) = input_max;
-        
-        run("half_model_calculation.m")
-        pitch_inte_list(loop) = pitch_integral;
-        pitch_max_list(loop) = pitch_max;
-        input_inte_list(loop) = input_integral;
-        input_max_list(loop) = input_max;
+%         run("half_model_calculation.m")
+%         pitch_inte_list_onlylpf(loop) = pitch_integral;
+%         pitch_max_list_onlylpf(loop) = pitch_max;
+%         input_inte_list_onlylpf(loop) = input_integral;
+%         input_max_list_onlylpf(loop) = input_max;
+%         
+%         run("half_model_calculation.m")
+%         pitch_inte_list(loop) = pitch_integral;
+%         pitch_max_list(loop) = pitch_max;
+%         input_inte_list(loop) = input_integral;
+%         input_max_list(loop) = input_max;
     end
     save("nonfiltered/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list_nonfilter","pitch_max_list_nonfilter","input_inte_list_nonfilter","input_max_list_nonfilter");
-    save("onlylpf/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list_onlylpf","pitch_max_list_onlylpf","input_inte_list_onlylpf","input_max_list_onlylpf");
-    save("filtered/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list","pitch_max_list","input_inte_list","input_max_list");
+%     save("onlylpf/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list_onlylpf","pitch_max_list_onlylpf","input_inte_list_onlylpf","input_max_list_onlylpf");
+%     save("filtered/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list","pitch_max_list","input_inte_list","input_max_list");
 end
 save("total_variables.mat");
