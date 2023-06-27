d12ced0863dd89704c03c234f06f6eb114314cec
Masato <masato_7121@keio.jp>
2023-06-27 13:44:31 +0900

    Add: error set maker

---
+++ half_active/monte_carlo/make_errors.m
@@ -0,0 +1,19 @@
+load("../prediction_error/variances.mat")
+
+rng(0,'twister');
+trend_std = sqrt(trend_error_var);
+height_std = sqrt(height_error_var);
+noise_std = sqrt(sensor_noise_var);
+
+trend_error_list = round(deg2rad(trend_std)*randn(1,200),1);
+height_error_list = round(height_std*randn(1,200),3);
+sensor_noise_list = round(noise_std*randn(1,200),3);
+
+idx = randi(200,[100,500,2]);
+
+plane_errors_set = zeros([100,500,2]);
+
+for i = 1:80
+    plane_errors_set(i,:,1) = trend_error_list(reshape(idx(i,:,1),1,500));
+    plane_errors_set(i,:,2) = height_error_list(reshape(idx(i,:,2),1,500));
+end
--- half_active/monte_carlo/monte_sim.m
+++ half_active/monte_carlo/monte_sim.m
@@ -1,4 +1,4 @@
-monte_iter_num = 1000;
+monte_iter_num = 100;
 
 ld_list = [0.5, 1, 2, 2.5];
 max_z0_list = [0.01, 0.045, 0.08];
@@ -39,7 +39,7 @@ for conz = 1:width(max_z0_list)
             end
             wa  = false;
             lpf = false;
-            run("half_model_calculation.m")
+            run("half_model_calc_monte.m")
             pitch_inte_list_nonfilter(1,loop) = pitch_integral;
             pitch_max_list_nonfilter(1,loop) = pitch_max;
             input_inte_list_nonfilter(1,loop) = input_integral;
@@ -49,7 +49,7 @@ for conz = 1:width(max_z0_list)
             
             wa  = false;
             lpf = true;
-            run("half_model_calculation.m")
+            run("half_model_calc_monte.m")
             pitch_inte_list_onlylpf(1,loop) = pitch_integral;
             pitch_max_list_onlylpf(1,loop) = pitch_max;
             input_inte_list_onlylpf(1,loop) = input_integral;
@@ -59,7 +59,7 @@ for conz = 1:width(max_z0_list)
 
             wa  = true;
             lpf = true;
-            run("half_model_calculation.m")
+            run("half_model_calc_monte.m")
             pitch_inte_list(1,loop) = pitch_integral;
             pitch_max_list(1,loop) = pitch_max;
             input_inte_list(1,loop) = input_integral;
@@ -67,9 +67,9 @@ for conz = 1:width(max_z0_list)
             wf_global_filter(loop) = {[wf_global(1,:)+T*V; wf_global(2,:)]};
             clear wf_global;
         end
-        save("nonfiltered_specify/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list_nonfilter","pitch_max_list_nonfilter","input_inte_list_nonfilter","input_max_list_nonfilter","wf_global_nonfilter","r_p_prev");
-        save("onlylpf_specify/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list_onlylpf","pitch_max_list_onlylpf","input_inte_list_onlylpf","input_max_list_onlylpf","wf_global_onlylpf","r_p_prev");
-        save("filtered_specify/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list","pitch_max_list","input_inte_list","input_max_list","wf_global_filter","r_p_prev");
+        save("nonfiltered_all/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list_nonfilter","pitch_max_list_nonfilter","input_inte_list_nonfilter","input_max_list_nonfilter","wf_global_nonfilter","r_p_prev");
+        save("onlylpf_all/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list_onlylpf","pitch_max_list_onlylpf","input_inte_list_onlylpf","input_max_list_onlylpf","wf_global_onlylpf","r_p_prev");
+        save("filtered_all/ld="+ld+"-max_z0="+max_z0+".mat","pitch_inte_list","pitch_max_list","input_inte_list","input_max_list","wf_global_filter","r_p_prev");
     end
 end
 save("total_variables.mat");
