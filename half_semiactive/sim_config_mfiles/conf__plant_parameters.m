295dbe803c8dc831d331dd199bc4831746bf96ca
INOUE MASATO <masato_7121@keio.jp>
2023-07-30 02:10:03 +0900

    Fixed: newest version

---
+++ half_semiactive/sim_config_mfiles/conf__plant_parameters.m
@@ -0,0 +1,22 @@
+%% Vehicle parameter
+m_b = 1000;     % [kg]      body mass
+m_w = 30;       % [kg]      wheel mass
+k_sf = 33000;   % [N/m]     front spring stiffness
+k_sr = 37000;   % [N/m]     rear spring stiffness
+% k_sf = 13000;   % [N/m]     front spring stiffness
+% k_sr = 17000;   % [N/m]     rear spring stiffness
+k_w = 270000;   % [N/m]     tire stiffness
+% k_w = 60000;   % [N/m]     tire stiffness
+c_sf = 3300;    % [N/(m/s)] front damping
+c_sr = 3250;    % [N/(m/s)] rear damping
+c_w = 1000;       % [N/(m/s)] tire damping
+% c_w = 2000;       % [N/(m/s)] tire damping
+I_b = 750;      % [kgm^2]   inertia moment
+L_f = 1.4;      % [m]       front length
+L_r = 1.4;      % [m]       rear length
+
+cam_fwheel_dis = 0.895;
+
+Vkm_h = 50;     % [km/h]    driving velocity
+Vkm_m=Vkm_h/60;
+V=Vkm_m*1000/60;% [m/s]
---
+++ half_semiactive/sim_config_mfiles/conf__rpf_settings.m
@@ -0,0 +1,39 @@
+
+TL = 0:dt:T;     % time list ([s])
+dis = 0:T*V/(T/dt):T*V;                                                       % distance time line ([m])
+start_disturbance = 9;                                                        % the start distance of disturbance ([m])
+
+max_z0 = 0.08;                                                                % [m] max road displacement
+ld = 2;
+const = 6;                                                                    % amplitude
+max_distance = 300;                                                           % [m] driving mileage
+dis_total = 0:max_distance/(T/dt):max_distance;                               % distance list for road profile ([m])
+dis_total_f = 0:max_distance/(T/dt):max_distance-start_disturbance;                           % distance list for front road profile ([m])
+dis_total_r = 0:max_distance/(T/dt):max_distance-start_disturbance-(L_f+L_r);                 % distance list for rear road profile ([m])
+
+[road_total_f,road_total_r,ld,frequency,max_z0,dis_length] = road_prof_maker(shape,TL,T,dt,V,L_f,L_r,dis,start_disturbance,max_z0,ld,const,max_distance,dis_total,dis_total_f,dis_total_r);
+
+if sensing
+    r_p_f = interp1(road_total_f(:,1)',road_total_f(:,2)',dis,'linear');                                   % front road profile
+    r_p_r = interp1(road_total_r(:,1)',road_total_r(:,2)',dis,'linear');                                   % rear road profile
+    r_p_f = lowpass(r_p_f,0.5,1/dt);
+    r_p_r = lowpass(r_p_r,0.5,1/dt);
+else
+    r_p_f = makima(dis_total,road_total_f,dis);                                   % front road profile
+    r_p_r = makima(dis_total,road_total_r,dis);                                   % rear road profile
+end
+
+
+r_p = [
+    r_p_f;
+    r_p_r;
+    gradient(r_p_f)./dt;
+    gradient(r_p_r)./dt
+    ];
+r_p_prev = [
+    dis;
+    r_p_f;
+    r_p_r;
+    gradient(r_p_f)./dt;
+    gradient(r_p_r)./dt
+    ];
