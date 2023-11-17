0d7af44267fd5b4dc7280e0e64915225f67accae
INOUE MASATO <masato_7121@keio.jp>
2023-11-17 21:23:23 +0900

    dem gradient implementation

--- half_active/road_dem.m
+++ half_active/road_dem.m
@@ -1,5 +1,40 @@
-depth = 8;
+depth = 6;
 mesh2 = pc2surfacemesh(pointCloud(ptc_datas),"poisson",depth);
-surfaceMeshShow(mesh2)
+% surfaceMeshShow(mesh2)
+% writeSurfaceMesh(mesh2,"test_surface");
 figure
-pcshow(mesh2.Vertices)
+pcshow(mesh2.Vertices); hold on;
+
+%% pointcloud interpolation
+xlist = -1:0.1:1;
+ylist = 3:0.1:12;
+[xq,yq] = meshgrid(xlist,ylist);
+elevation_mesh = griddata(datas(:,1),datas(:,2),datas(:,3),xq,yq,"cubic");
+figure;
+surf(xq,yq,elevation_mesh);
+axis equal
+% colormap default
+
+%% surface gradient
+[fx,fy] = gradient(elevation_mesh,0.1);
+figure;
+
+z_vec = [0,0,1];                        % z vector
+angle_r = subspace(z_vec',u_vec');          % [rad] angle
+gradient_mesh = surf(xq,yq,abs(fy)+abs(fx));
+axis equal
+% colormap(autumn(5))
+
+%% draw
+figure;
+tiledlayout('flow')
+ax1 = nexttile;
+pcshow(mesh2.Vertices);
+%colormap(ax1,winter)
+ax2 = nexttile;
+mesh(xq,yq,elevation_mesh);
+axis equal
+colormap(ax2,autumn(5))
+nexttile([2,2]);
+gradient_mesh = mesh(xq,yq,fy);
+axis equal
--- half_active/sim_config_mfiles/conf__simulation_conditions.m
+++ half_active/sim_config_mfiles/conf__simulation_conditions.m
@@ -1,6 +1,6 @@
 %% Dfine simulation condition with boolean
 % draw an animation or not
-animation = true;
+animation = false;
 prev_anim = false;
 
 % add noise or not
@@ -20,8 +20,8 @@ fprev_rprev     = false;
 LQR_fprev_rprev = false;
 
 % road profile
-sensing = false;
-paper   = true;
+sensing = true;
+paper   = false;
 sin_wave = false;
 step    = false;
 manhole = false;
