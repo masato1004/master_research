de7e188f32e025c4647968ffa26f11896260b654
INOUE MASATO <masato_7121@keio.jp>
2023-12-11 18:19:19 +0900

    Add: tsnd151

---
+++ sensing/tsnd151/ad_detector.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# TSDN sample program for Python3
+#
+# required:
+#     PySerial
+#
+import os
+import sys
+import serial
+import time
+import struct
+import binascii
+import ctypes
