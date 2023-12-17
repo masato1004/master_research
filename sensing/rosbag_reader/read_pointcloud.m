3b347c7b82fbca531ed0fbf5e349b5a2045845b1
INOUE MASATO <masato_7121@keio.jp>
2023-12-17 17:06:36 +0900

    Add: osbag, zedbag reader

--- sensing/rosbag_reader/read_pointcloud.m
+++ sensing/rosbag_reader/read_pointcloud.m
@@ -1,25 +1,33 @@
+% ros time resolution is 1e-6
 bag = rosbag("E:\nissan\20231209_calibration\_2023-12-09-16-57-02.bag")
-pcbag = select(bag,'Topic','/zed2i/zed_node/point_cloud/cloud_registered')
-pcMsgs = readMessages(pcbag);
-ts = timeseries(pcbag)
-t = ts.Time;
-pcFilesPath = fullfile(tempdir,'PointClouds');
+osbag = select(bag,'Topic','/ouster/points')
+osMsgs = readMessages(osbag);
+osts = timeseries(osbag)
+ost = osts.Time;
 
-if ~exist(pcFilesPath,'dir')
-    mkdir(pcFilesPath);
-end
-
-for i = 1:length(t)
-    pc = pointCloud(readXYZ(pcMsgs{i}));
-    n_strPadded = sprintf('%04d',i) ;
-    pcFileName = strcat(pcFilesPath,'/',n_strPadded,'.pcd');
-    pcwrite(pc,pcFileName);
-end
-
-pclist = dir("C:\Users\masato\AppData\Local\Temp\PointClouds\*.pcd");
-for i = 1:length(pclist)
-    filename = pclist(i).folder + "/" + pclist(i).name;
-    ptcloud = pcread(filename);
-    pcshow(ptcloud);
+zedbag = select(bag,'Topic','/zed2i/zed_node/point_cloud/cloud_registered')
+zedMsgs = readMessages(zedbag);
+zedts = timeseries(zedbag)
+zedt = osts.Time;
+% pcFilesPath = fullfile(tempdir,'PointClouds');
+% 
+% if ~exist(pcFilesPath,'dir')
+%     mkdir(pcFilesPath);
+% end
+display("start");
+for i = 1:length(ost)
+    pc = pointCloud(readXYZ(osMsgs{i}));
+    pcshow(pc);
     drawnow
+    % n_strPadded = sprintf('%04d',i) ;
+    % pcFileName = strcat(pcFilesPath,'/',n_strPadded,'.pcd');
+    % pcwrite(pc,pcFileName);
 end
+
+% pclist = dir("C:\Users\masato\AppData\Local\Temp\PointClouds\*.pcd");
+% for i = 1:length(pclist)
+%     filename = pclist(i).folder + "/" + pclist(i).name;
+%     ptcloud = pcread(filename);
+%     pcshow(ptcloud);
+%     drawnow
+% end
