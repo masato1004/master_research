146dfaae27347ba8b5a680f6ff145a9315aa1e64
INOUE MASATO <masato_7121@keio.jp>
2024-04-03 19:39:59 +0900

    WIP: Lagrange

--- lagrange/motion_equation_konishi_M01.m
+++ lagrange/motion_equation_konishi_M01.m
@@ -1,22 +1,33 @@
 %%%      Motion Equation (Linear)    %%%
 clear
 
-syms x_1 dx_1 ddx_1
-syms x_2 dx_2 ddx_2
-syms x_4 dx_4 ddx_4
+syms x_b dx_b ddx_b        % body longitudinal
+syms z_b dz_b ddz_b        % body vertical
+syms z_wf dz_wf ddz_wf     % fornt wheel vertical
+syms z_wr dz_wr ddz_wr     % rear wheel vertical
+syms theta dtheta ddtheta  % body rotation
 
-syms m1 m2 m3 m4  
-syms k1 k3 k4
-syms d1 d3 d4 
+syms x_disf dx_disf ddx_disf  % road longitudinal
+syms x_disr dx_disr ddx_disr  % road longitudinal
+syms z_disf dz_disf ddz_disf  % road displacement
+syms z_disr dz_disr ddz_disr  % road displacement
+
+syms m_b m_wf m_wr I_b
+syms k_sf k_sr k_wf k_wr k_longf k_longr
+syms c_sf c_sr c_wf c_wr c_longf c_longr
 
 %%% Energy %%%
-syms KE UE DE
+syms KE PE DE
 %%% Common %%%
-syms t g u
+syms t g u Lf Lr
 
-q = [ x_1 x_2 ].';
-dq = [ dx_1 dx_2  ].';
-ddq = [ ddx_1 ddx_2  ].';
+m_list   = [ m_b m_b m_wf m_wr I_b ].';
+k_list   = [ k_longf k_longr k_sf k_sr k_wf k_wr ].';
+c_list   = [ c_longf c_longr c_sf c_sr c_wf c_wr ].';
+
+q   = [ x_b z_b z_wf z_wr theta ].';
+dq  = [ dx_b dz_b dz_wf dz_wr dtheta ].';
+ddq = [ ddx_b ddz_b ddz_wf ddz_wr ddtheta ].';
 
 % Xb = [ x_b  z_b].';
 % Xf = [ x_tf  z_b+lf*sin(q(7,1)) ].';
@@ -26,13 +37,13 @@ ddq = [ ddx_1 ddx_2  ].';
 % dXf = jacobian(Xf,q)*dq;
 % dXr = jacobian(Xr,q)*dq;
 
-KE = simplify( 1/2*m1*dq(1,1)^2 + 1/2*(m2+m3)*dq(2,1)^2 );
+KE = simplify( sum(1/2*(m_list.*dq.^2)) );
 KE = simplify(KE);
 
-PE = simplify( 1/2*k1*(q(1,1)-q(2,1))^2 + 1/2*k3*q(2,1)^2 );
+PE = simplify( 1/2*(k_list(1,1)*(q(1,1)-x_disf)^2 + k_list(2,1)*(q(1,1)-x_disr)^2 + k_list(3,1)*(q(2,1)+q(5,1)*Lf-q(3,1))^2 + k_list(4,1)*(q(2,1)-q(5,1)*Lr-q(4,1))^2 + k_list(5,1)*(q(3,1)-z_disf)^2 + k_list(6,1)*(q(4,1)-z_disr)^2) );
 PE = simplify(PE);
 
-DE = simplify( 1/2*d1*(dq(1,1)-dq(2,1))^2 + 1/2*d3*dq(2,1)^2 );
+DE = simplify( 1/2*(c_list(1,1)*(dq(1,1)-dx_disf)^2 + c_list(2,1)*(dq(1,1)-dx_disr)^2 + c_list(3,1)*(dq(2,1)+dq(5,1)*Lf-dq(3,1))^2 + c_list(4,1)*(dq(2,1)-dq(5,1)*Lr-dq(4,1))^2 + c_list(5,1)*(dq(3,1)-dz_disf)^2 + c_list(6,1)*(dq(4,1)-dz_disr)^2) );
 DE = simplify(DE);
 
 %%% M(q) ddq + C(q,dq) dq + K(q) q = B u %%%
