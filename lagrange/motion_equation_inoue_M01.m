8ef70250339dfd2d0f10892bb43d4bfb9e4f7b67
INOUE MASATO <masato_7121@keio.jp>
2024-04-12 17:19:46 +0900

    WIP: conf__conf_state_space.m

--- lagrange/motion_equation_inoue_M01.m
+++ lagrange/motion_equation_inoue_M01.m
@@ -21,7 +21,7 @@ syms alpha_f alpha_r sus_f sus_r
 %%% Energy %%%
 syms KE PE DE
 %%% Common %%%
-syms t g Lf Lr S r
+syms t g L_f L_r S r
 
 m_list   = [ m_b m_b m_wf m_wr I_b ].';
 k_list   = [ k_longf k_longr k_sf k_sr k_wf k_wr ].';
@@ -33,9 +33,11 @@ ddq = [ ddx_b ddz_b ddz_wf ddz_wr ddtheta ].';
 
 w  = [ x_disf, x_disr, z_disf, z_disr, dx_disf, dx_disr, dz_disf, dz_disr ].';
 
+u  = [ alpha_f alpha_r sus_f sus_r ].';
+
 % Xb = [ x_b  z_b].';
-% Xf = [ x_tf  z_b+lf*sin(q(7,1)) ].';
-% Xr = [ x_tr  z_b-lr*sin(q(7,1)) ].';
+% Xf = [ x_tf  z_b+L_f*sin(q(7,1)) ].';
+% Xr = [ x_tr  z_b-L_r*sin(q(7,1)) ].';
 % 
 % dXb = jacobian(Xb,q)*dq;
 % dXf = jacobian(Xf,q)*dq;
@@ -44,10 +46,10 @@ w  = [ x_disf, x_disr, z_disf, z_disr, dx_disf, dx_disr, dz_disf, dz_disr ].';
 KE = simplify( sum(1/2*(m_list.*dq.^2)) );
 KE = simplify(KE);
 
-PE = simplify( 1/2*(k_list(1,1)*(q(1,1)-x_disf)^2 + k_list(2,1)*(q(1,1)-x_disr)^2 + k_list(3,1)*(q(2,1)+q(5,1)*Lf-q(3,1))^2 + k_list(4,1)*(q(2,1)-q(5,1)*Lr-q(4,1))^2 + k_list(5,1)*(q(3,1)-z_disf)^2 + k_list(6,1)*(q(4,1)-z_disr)^2) );
+PE = simplify( 1/2*(k_list(1,1)*(q(1,1)-x_disf)^2 + k_list(2,1)*(q(1,1)-x_disr)^2 + k_list(3,1)*(q(2,1)+q(5,1)*L_f-q(3,1))^2 + k_list(4,1)*(q(2,1)-q(5,1)*L_r-q(4,1))^2 + k_list(5,1)*(q(3,1)-z_disf)^2 + k_list(6,1)*(q(4,1)-z_disr)^2) );
 PE = simplify(PE);
 
-DE = simplify( 1/2*(c_list(1,1)*(dq(1,1)-dx_disf)^2 + c_list(2,1)*(dq(1,1)-dx_disr)^2 + c_list(3,1)*(dq(2,1)+dq(5,1)*Lf-dq(3,1))^2 + c_list(4,1)*(dq(2,1)-dq(5,1)*Lr-dq(4,1))^2 + c_list(5,1)*(dq(3,1)-dz_disf)^2 + c_list(6,1)*(dq(4,1)-dz_disr)^2) );
+DE = simplify( 1/2*(c_list(1,1)*(dq(1,1)-dx_disf)^2 + c_list(2,1)*(dq(1,1)-dx_disr)^2 + c_list(3,1)*(dq(2,1)+dq(5,1)*L_f-dq(3,1))^2 + c_list(4,1)*(dq(2,1)-dq(5,1)*L_r-dq(4,1))^2 + c_list(5,1)*(dq(3,1)-dz_disf)^2 + c_list(6,1)*(dq(4,1)-dz_disr)^2) );
 DE = simplify(DE);
 
 %%% M(q) ddq + C(q,dq) dq + K(q) q = B u %%%
@@ -72,10 +74,10 @@ K1 = simplify(jacobian(PE,q).');
 
 input_acc = [
     (m_wf+m_b/2)*(((1-S)*r*alpha_f - g*sin(atan(dz_disf/dx_disf)))*sin(atan(dz_disf/dx_disf)) + ((1-S)*r*alpha_r - g*sin(atan(dz_disr/dx_disr)))*sin(atan(dz_disr/dx_disr)))/(2*m_wf + m_b);
-    -g;
-    ((1-S)*r*alpha_f - g*sin(atan(dz_disf/dx_disf)))*cos(atan(dz_disf/dx_disf));
-    ((1-S)*r*alpha_r - g*sin(atan(dz_disr/dx_disr)))*cos(atan(dz_disr/dx_disr));
-    0
+    -g + sus_f + sus_r;
+    ((1-S)*r*alpha_f - g*sin(atan(dz_disf/dx_disf)))*cos(atan(dz_disf/dx_disf)) - sus_f;
+    ((1-S)*r*alpha_r - g*sin(atan(dz_disr/dx_disr)))*cos(atan(dz_disr/dx_disr)) - sus_r;
+    L_f*sus_f - L_r*sus_r
 ];
 input_acc = simplify(input_acc);
 
@@ -90,17 +92,10 @@ input_acc = simplify(input_acc);
 % sus_f
 % sus_r
 
-B1 = [
-       0,0;
-    0,0   ;
-    0,0   ;
-    0,0   ;
-    0,0
-    ];
 
 
 disp(' ')
-disp(' M*ddq + C*dq + K*q = B*u + E3*w ')
+disp(' M*ddq + C*dq + K*q = B*u + E*w ')
 disp(' ')
 
 % M
@@ -117,13 +112,17 @@ for j=1:size(w,1)
     E1(:,j) = diff(-K1, w(j,1));
     E2(:,j) = diff(-C3, w(j,1));
 end
+for k=1:size(u,1)
+    B(:,k) = diff(input_acc, u(k,1));
+end
 
 E = simplify( E1 + E2 );
 
 %%% Amat*X + Bmat*u = Emat*F %%%
 %%% Ymat = Cmat*X + Dmat*u %%%
-% Amat = [zeros(size(M,1)) eye(size(M,1)); -M\K  -M\C]
-% Bmat = [zeros(size(M,1),width(B));M\B1]
+Amat = [zeros(size(M,1)) eye(size(M,1)); -M\K  -M\C];
+Bmat = [zeros(size(M,1),width(B));M\B];
 Emat = [zeros(size(M,1),width(E));M\E];
-% Cmat = eye(size(Amat,1))
+Cmat = eye(size(Amat,1));
+save('../lng_modeling/half_active/system_matrices', 'Amat', 'Bmat', 'Emat', 'Cmat');
 % Dmat = []
---
+++ lng_modeling/half_active/conf__state_space.m
@@ -0,0 +1,81 @@
+%% Load system matrices from the file
+% syms x_b dx_b ddx_b        % body longitudinal
+% syms z_b dz_b ddz_b        % body vertical
+% syms z_wf dz_wf ddz_wf     % fornt wheel vertical
+% syms z_wr dz_wr ddz_wr     % rear wheel vertical
+% syms theta dtheta ddtheta  % body rotation
+% 
+% syms x_disf dx_disf ddx_disf  % road longitudinal
+% syms x_disr dx_disr ddx_disr  % road longitudinal
+% syms z_disf dz_disf ddz_disf  % road displacement
+% syms z_disr dz_disr ddz_disr  % road displacement
+% 
+% syms m_b m_wf m_wr I_b
+% syms k_sf k_sr k_wf k_wr k_longf k_longr
+% syms c_sf c_sr c_wf c_wr c_longf c_longr
+% 
+% syms alpha_f alpha_r sus_f sus_r
+% 
+% syms t g L_f L_r S r
+
+% run('../../lagrange/motion_equation_inoue_M01.m')  % only for the first time or when it was changed
+load('system_matrices.mat');
+
+%% Discretization
+disc_func = @(tau,Mat) (-Amat\expm(Amat.*(tc-tau)))*Mat;
+
+A = expm(Amat.*tc);
+B = disc_func(tc,Bmat) - disc_func(0,Bmat);
+E = disc_func(tc,Emat) - disc_func(0,Emat);
+
+CA = Cmat*A;
+CB = Cmat*B;
+CE = Cmat*E;
+
+% dx(k) = x(k) - x(k-1)
+% X(k) = phi*dx(k) + G*du(k) + Gd*dw(k)
+phi = [
+    eye(height(CA)), -CA;
+    zeros(width(CA),height(CA)), A
+    ];
+
+G = [
+    -CB;
+    B
+    ];
+
+Gd = [
+    -CE;
+    E
+    ];
+
+%% Vehicle parameter
+m_b = 960;     % [kg]      body mass
+m_wf = 40;       % [kg]      wheel mass
+m_wr = m_wf;       % [kg]      wheel mass
+k_sf = 30000;   % [N/m]     front spring stiffness
+k_sr = 25000;   % [N/m]     rear spring stiffness
+% k_sf = 13000;   % [N/m]     front spring stiffness
+% k_sr = 17000;   % [N/m]     rear spring stiffness
+k_wf = 270000;   % [N/m]     tire stiffness
+k_wr = k_wf;   % [N/m]     tire stiffness
+% k_w = 60000;   % [N/m]     tire stiffness
+c_sf = 7000;    % [N/(m/s)] front damping
+c_sr = 6000;    % [N/(m/s)] rear damping
+c_wf = 1000;       % [N/(m/s)] tire damping
+c_wr = c_wf;       % [N/(m/s)] tire damping
+% c_w = 2000;       % [N/(m/s)] tire damping
+wb = 2.45;     % wheel base
+L_f = wb * 23/55;      % [m]       front length
+L_r = wb * 32/55;      % [m]       rear length
+I_b = m_b*((wb)/2)^2;      % [kgm^2]   inertia moment
+
+Vkm_h = 10;     % [km/h]    driving velocity
+Vkm_m=Vkm_h/60;
+V=Vkm_m*1000/60;% [m/s]
+
+%% Load parameters into matrices
+Ap = subs(Amat);
+Bp = subs(Bmat);
+Ep = subs(Emat);
+C = subs(Cmat);
