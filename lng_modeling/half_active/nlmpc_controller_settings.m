7d19aa7c1b5f5076123be50154922dc4964f454c
masato1004 <masato_7121@keio.jp>
2024-04-30 06:07:46 +0900

    CHANGE: file where validateFcns

--- lng_modeling/half_active/nlmpc_controller_settings.m
+++ lng_modeling/half_active/nlmpc_controller_settings.m
@@ -31,6 +31,14 @@ runner.UseMVRate = true;
 
 simdata = getSimulationData(runner);
 size(simdata.StageParameter)
-% validateFcns(runner,rand(height(states),1),rand(height(u),1),simdata);
 
-options = nlmpcmoveopt;
+current_mileage_f = makima(dis_total-disturbance(1,1),mileage_f-makima(dis_total,mileage_f,disturbance(1,1)),0:Ts*states(8,1):Ts*states(8,1)*(pHorizon+9));
+current_mileage_r = makima(dis_total-disturbance(2,1),mileage_r-makima(dis_total,mileage_r,disturbance(2,1)),0:Ts*states(8,1):Ts*states(8,1)*(pHorizon+9));
+current_wheel_traj_f = makima(wheel_traj_f(1,:),wheel_traj_f(2,:),disturbance(1,1):Ts*states(8,1):Ts*states(8,1)*(pHorizon+9)+disturbance(1,1));
+current_wheel_traj_r = makima(wheel_traj_r(1,:),wheel_traj_r(2,:),disturbance(2,1):Ts*states(8,1):Ts*states(8,1)*(pHorizon+9)+disturbance(2,1));
+simdata.StateFcnParameter = [disturbance(:,1);[0:Ts*states(8,1):Ts*states(8,1)*(pHorizon+9)].';current_mileage_f.';current_mileage_r.';current_wheel_traj_f.';current_wheel_traj_r.'];
+simdata.StageParameter = repmat([simdata.StateFcnParameter; nlmpc_config__referenceSignal(states(:,1),u(:,1),V,Ts)],pHorizon+1,1);
+validateFcns(runner,states(:,1),u(:,1),simdata);
+disp('validate done.')
+
+% options = nlmpcmoveopt;
