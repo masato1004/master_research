3e27e5b3defc0b4b393fac655ad0c71f4eef8d3a
masato1004 <masato_7121@keio.jp>
2024-05-26 00:25:24 +0900

    ADD: depth to pointcloud

---
+++ sensing/rosbag_reader/ouster-dual/depth2ptc.m
@@ -0,0 +1,13 @@
+file_name = "1708413010.220381.png";
+depthImage = imread("depth_imgs/"+file_name);
+colorImage = imread("color_imgs/"+file_name);
+
+focalLength      = [1070.6, 1070.58];
+principalPoint   = [964.41, 547.833];
+imageSize        = size(depthImage,[1,2]);
+intrinsics       = cameraIntrinsics(focalLength,principalPoint,imageSize);
+depthScaleFactor = 65535/20;
+maxCameraDepth   = 20;
+
+ptCloud = pcfromdepth(depthImage,depthScaleFactor,intrinsics,ColorImage=colorImage)
+pchsow(ptCloud);
