84a62465f2ee70df5f1d529ab7f1a1748795e7b5
masato1004 <masato_7121@keio.jp>
2024-05-26 01:14:34 +0900

    ADD: comment out

--- sensing/rosbag_reader/ouster-dual/depth2ptc.m
+++ sensing/rosbag_reader/ouster-dual/depth2ptc.m
@@ -4,9 +4,9 @@ colorImage = imread("color_imgs/"+file_name);
 
 focalLength      = [1070.6, 1070.58];
 principalPoint   = [964.41, 547.833];
-imageSize        = size(depthImage,[1,2]);
+imageSize        = size(colorImage,[1,2]);
 intrinsics       = cameraIntrinsics(focalLength,principalPoint,imageSize);
-depthScaleFactor = 65535/20;
+depthScaleFactor = 65535/20; % Z = 深度画像[u,v]/深度スケール係数 ここが違う
 maxCameraDepth   = 20;
 
 ptCloud = pcfromdepth(depthImage,depthScaleFactor,intrinsics,ColorImage=colorImage);
