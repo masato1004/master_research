097ec8cdc3ab38ad14b208840ab9a5cffb924a29
Masato <92346511+masato1004@users.noreply.github.com>
2024-05-26 13:08:56 +0900

    ADD: Distortions as camera intrinsic

--- sensing/rosbag_reader/ouster-dual/depth2ptc.m
+++ sensing/rosbag_reader/ouster-dual/depth2ptc.m
@@ -5,9 +5,11 @@ colorImage = imread("color_imgs/"+file_name);
 focalLength      = [1070.6, 1070.58];
 principalPoint   = [964.41, 547.833];
 imageSize        = size(colorImage,[1,2]);
-intrinsics       = cameraIntrinsics(focalLength,principalPoint,imageSize);
-depthScaleFactor = 65535/20; % Z = 深度画像[u,v]/深度スケール係数 ここが違う
-maxCameraDepth   = 20;
+RadialDistortion = [-0.0546184,0.0269859,-0.0105149];
+TangentialDistortion = [3.24293e-05,-0.000156357];
+intrinsics       = cameraIntrinsics(focalLength,principalPoint,imageSize,"RadialDistortion",RadialDistortion,"TangentialDistortion",TangentialDistortion);
+depthScaleFactor = 65535/15; % Z = 深度画像[u,v]/深度スケール係数 ここが違う
+maxCameraDepth   = 15;
 
 ptCloud = pcfromdepth(depthImage,depthScaleFactor,intrinsics,ColorImage=colorImage);
 pcshow(ptCloud);
