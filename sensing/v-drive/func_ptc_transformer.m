93d43a01c96ec7ec674901a371f7f2ac49042aa0
masato1004 <masato_7121@keio.jp>
2024-11-07 16:00:21 +0900

    ADD: v-drive proto folder to sensing

---
+++ sensing/v-drive/func_ptc_transformer.m
@@ -0,0 +1,20 @@
+function [depth_info_on_img,cameraPoints] =  func_ptc_transformer(pcd,camIntrinsics,tmat_cam2lidar)
+    % transform into image coordinate
+    eulerAngle2 = [0 -pi/2 pi/2];
+    R2 = eul2rotm(eulerAngle2);
+    A2 = [[R2;0,0,0],[0;0;0;1]];
+    
+    % A3 = A2*tmat_cam2lidar;
+
+    tform_campcd = rigidtform3d(tmat_cam2lidar);
+    campcd = pctransform(pcd,tform_campcd);
+    
+    tform = rigidtform3d(A2);
+    [imPts,indices] = projectLidarPointsOnImage(campcd,camIntrinsics,tform);
+    cameraPoints = pctransform(campcd,tform);
+
+    % depth = sqrt(campcd.Location(indices,1).^2+campcd.Location(indices,2).^2+campcd.Location(indices,3).^2);
+    depth = cameraPoints.Location(indices,3);
+    depth_info_on_img = [imPts,depth];
+
+end
---
+++ sensing/v-drive/proto_ptc_transformer.m
@@ -0,0 +1,94 @@
+close all;
+
+%% define pcd and img
+lidar_name = "roof"
+
+pcd_dir_name = "ply_"+lidar_name+"-lidar_tmp";
+img_dir_name = "image_2";
+
+pcd_list = dir(pcd_dir_name+"/*.pcd");
+img_list = dir(img_dir_name+"/*.png");
+
+if ~exist("imageNdepth",'dir')
+    mkdir("imageNdepth")
+end
+
+%% video setting
+videoname = "imageNdepth"+"/test"+lidar_name;
+video = VideoWriter(videoname,'MPEG-4');
+video.FrameRate = 20;
+open(video);
+
+%% camera parameter
+img_w = 3840;
+img_h = 2160;
+k1 = 1.36648;
+k2 = 1.79417;
+k3 = 0.1704;
+k4 = 1.90693;
+k5 = 2.64875;
+k6 = 0.97058;
+p1 = 0.00014;
+p2 = -0.00008;
+fx = 2445.66438;
+fy = 2444.75377;
+cx = 1905.44853;
+cy = 1073.60153;
+imageSize = [img_h, img_w];
+focalLength      = [fx, fy];
+principalPoint   = [cx, cy];
+RadialDistortion = [-0.06390521968364953,0.0013426527038489695,-8.135414576415314e-06];
+RadialDistortion6 = [k1, k2, k3, k4, k5, k6];
+TangentialDistortion = [p1,p2];
+intrinsics       = cameraIntrinsics(focalLength,principalPoint,imageSize,"RadialDistortion",RadialDistortion,"TangentialDistortion",TangentialDistortion);
+
+%% cam2roof-lidar transformation
+lidar_position_r = [1.110943, 0., 1.998877];
+lidar_angles_r = [0., 0., 0.];
+lidar_position_f = [3.639492, 0., 0.662594];
+lidar_angles_f = [0., deg2rad(22.500000), 0.];
+
+lidar_position = lidar_position_r;
+lidar_angles   =   lidar_angles_r;
+camera_position = [1.690000, 0.0, 1.500000];
+camera_angles = [0., 0., 0.];
+
+%% figure
+img_fig = figure();
+for i = 1:length(pcd_list)
+    pcd_name = pcd_list(i).name;
+    img_name = img_list(i).name;
+    
+    %% load img
+    img = imread(img_dir_name+"/"+img_name);
+    
+    %% imshow
+    imshow(img);
+    
+    if i > 1
+        %% load pcd
+        ptCloud_r = pcread(pcd_dir_name+"/"+pcd_name);
+        idx = sum(ptCloud_r.Location(:,:) ~= 0,2)~=0;
+        ptCloud_r = pointCloud(ptCloud_r.Location(idx,:));
+    
+        %% pcd on img
+        eulerAngle1 = lidar_angles;
+        translation =  - camera_position + lidar_position;
+        R1 = eul2rotm(eulerAngle1);
+        A1 = [[R1;0,0,0],[translation';1]];
+        
+        [depth_r,cameraPoints] = func_ptc_transformer(ptCloud_r,intrinsics,A1);
+        depth_r = func_projectLidarToDepthImage(cameraPoints.Location, focalLength, principalPoint, imageSize, RadialDistortion6, TangentialDistortion);
+        % pause(5)
+        %% show depth onto img
+        hold on
+        scatter(depth_r(:,1),depth_r(:,2),5,depth_r(:,3),'MarkerEdgeColor','flat','MarkerFaceColor','flat')
+        colormap(turbo);
+        clim([0 100])
+        hold off
+    end
+    drawnow;
+    frame = getframe(img_fig);
+    writeVideo(video,frame);
+end
+close(video)
