165f2ae3e05dc84759752ed790a75fb29c3cd32d
masato1004 <masato_7121@keio.jp>
2025-01-03 00:43:45 +0900

    ADD: slack bot

--- slack_notify.py
+++ slack_notify.py
@@ -3,7 +3,7 @@ from slack_sdk.errors import SlackApiError
 
 
 def SendToSlackMessage(message, username='PythonBot', icon_emoji=':thumbsup:'):
-    client = WebClient(token='xoxb-3710074566071-8240371641203-0bloVcXEKlp7EgngU80OokB5') 
+    client = WebClient(token='token_here') 
     response=client.chat_postMessage(channel='C07EN5NGKDZ', text=message, username=username, icon_emoji=icon_emoji)
 
 
